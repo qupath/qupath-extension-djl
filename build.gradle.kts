9af2196c466bbce23a381fccda852b2ec7193efb
Pete <pete.bankhead@gmail.com>
2025-11-11 17:07:49 +0000

    Update version, minor format change

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 qupathExtension {
 	name = "qupath-extension-djl"
-	version = "0.4.0"
+	version = "0.4.1-SNAPSHOT"
 	group = "io.github.qupath"
 	description = "QuPath extension to use Deep Java Library"
 	automaticModule = "qupath.extension.djl"
